4897ee5881583de76280afeda220bb5df82195b0
Ernesto <nv480@navaweb.com>
2021-02-13 01:58:06 -0600

    esta es una pagina web informativa

---
+++ scripts/effects.js
@@ -0,0 +1,50 @@
+window.sr = ScrollReveal()
+
+sr.reveal('.reveal-header', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+})
+
+sr.reveal('.reveal-title', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+})
+
+sr.reveal('.reveal-title', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-200px'
+})
+
+sr.reveal('.text-reveal', {
+    duration: 1800,
+    origin: 'right',
+    distance: '-200px'
+})
+
+sr.reveal('.cards-container', {
+    duration: 1950,
+    origin: 'top',
+    distance: '-400px'
+})
+
+sr.reveal('.brand1', {
+    duration: 1750,
+    origin: 'left',
+    distance: '-300px'
+})
+
+sr.reveal('.brand2', {
+    duration: 1750,
+    origin: 'right',
+    distance: '-300px'
+})
+
+
+sr.reveal('.social', {
+    duration: 1750,
+    origin: 'bottom',
+    distance: '-100px'
+})
